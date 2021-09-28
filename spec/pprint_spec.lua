27adc126e0b7139967e0caee1804ab3def711312
Ferronn <ferronn@ferronn.dev>
2021-09-27 21:57:25 -0500

    introduce pprint

---
+++ spec/pprint_spec.lua
@@ -0,0 +1,43 @@
+describe('pprint', function()
+
+  local pprint do
+    local fenv = {
+      assert = assert,
+      format = string.format,
+      ipairs = ipairs,
+      pairs = pairs,
+      sort = table.sort,
+      strrep = string.rep,
+      table = { concat = table.concat },
+      tinsert = table.insert,
+      type = type,
+    }
+    fenv._G = fenv
+    local addonenv = {}
+    setfenv(loadfile('src/pprint.lua'), fenv)('moo', addonenv)
+    pprint = addonenv.pprint
+  end
+
+  local tests = {
+    {
+      name = 'empty input',
+      input = {},
+      output = '{\n}\n',
+    },{
+      name = 'flat array',
+      input = {42, 'foo', 99, 'bar'},
+      output = '{\n\t42,\n\t"foo",\n\t99,\n\t"bar",\n}\n',
+    },{
+      name = 'flat string key dict',
+      input = { a = 42, b = 'foo' },
+      output = '{\n\t["a"] = 42,\n\t["b"] = "foo",\n}\n',
+    }
+  }
+
+  for _, t in ipairs(tests) do
+    it('works on ' .. t.name, function()
+      assert.same(t.output, pprint(t.input))
+    end)
+  end
+
+end)
---
+++ src/pprint.lua
@@ -0,0 +1,67 @@
+local _, G = ...
+
+local format, sort, strrep, tinsert = _G.format, _G.sort, _G.strrep, _G.tinsert
+
+local function isarray(t)
+  local i = 0
+  for _ in pairs(t) do
+    i = i + 1
+    if t[i] == nil then
+      return false
+    end
+  end
+  return true
+end
+
+function G.pprint(t)
+  local strs = {}
+  local function append(s)
+    tinsert(strs, s)
+  end
+  local level = 1
+  local function indent()
+    tinsert(strs, strrep('\t', level))
+  end
+  local pptkvs
+  local function pptv(v)
+    local ty = type(v)
+    if ty == 'number' then
+      append(format('%d,\n', v))
+    elseif ty == 'string' then
+      append(format('%q,\n', v))
+    elseif ty == 'table' then
+      append('{\n')
+      level = level + 1
+      pptkvs(v)
+      level = level - 1
+      indent()
+      append('},\n')
+    else
+      error('invalid table value type ' .. ty)
+    end
+  end
+  function pptkvs(tt)
+    if isarray(t) then
+      for _, v in ipairs(tt) do
+        indent()
+        pptv(v)
+      end
+    else
+      local keys = {}
+      for k in pairs(tt) do
+        assert(type(k) == 'string')
+        tinsert(keys, k)
+      end
+      sort(keys)
+      for _, k in ipairs(keys) do
+        indent()
+        append(format('[%q] = ', k))
+        pptv(tt[k])
+      end
+    end
+  end
+  tinsert(strs, '{\n')
+  pptkvs(t)
+  tinsert(strs, '}\n')
+  return table.concat(strs, '')
+end

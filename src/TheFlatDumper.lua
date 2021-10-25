8e97bd9500142ff82ee29ffd2c076b41dff75e77
Ferronn <ferronn@ferronn.dev>
2021-10-24 20:40:15 -0500

    revert TheFlatDumper to something simple

--- src/TheFlatDumper.lua
+++ src/TheFlatDumper.lua
@@ -3,25 +3,11 @@ local _, G = ...
 local buildinfo = { _G.GetBuildInfo() }
 local data = G.flatdump(_G)
 
-do
-  local f = _G.CreateFrame('Frame', nil, _G.UIParent, 'DialogBoxFrame')
-  f:SetPoint('CENTER')
-  f:SetSize(600, 100)
-  f:SetBackdrop({
-    bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background',
-    edgeFile = 'Interface\\PVPFrame\\UI-Character-PVP-Highlight',
-    edgeSize = 16,
-    insets = { left = 8, right = 6, top = 8, bottom = 8 },
+local frame = CreateFrame('Frame')
+frame:RegisterEvent('PLAYER_LOGOUT')
+frame:SetScript('OnEvent', function()
+  _G.TheFlatDumper = G.pprint({
+    BuildInfo = buildinfo,
+    Data = G.pprint(data),
   })
-  f:SetBackdropBorderColor(0, .44, .87, .5)
-
-  local eb = _G.CreateFrame('EditBox', nil, f)
-  eb:SetAllPoints()
-  eb:SetFontObject('ChatFontNormal')
-  eb:SetText(
-    'TheFlatDumperBuildInfo = ' ..  G.pprint(buildinfo) .. '\n' ..
-    'TheFlatDumperData = ' .. G.pprint(data) .. '\n')
-  eb:HighlightText()
-  eb:SetCursorPosition(0)
-  f:Show()
-end
+end)
